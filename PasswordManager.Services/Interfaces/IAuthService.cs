f164705720d1b67fa0bd4f83d54b2b1a126e20c9
Sead Xhigoli <sead.xhigoli@outlook.com>
2024-11-10 14:15:49 +0100

    starting the registration implementation

--- PasswordManager.Services/Interfaces/IAuthService.cs
+++ PasswordManager.Services/Interfaces/IAuthService.cs
@@ -6,7 +6,7 @@ namespace PasswordManager.Services.Interfaces
     public interface IAuthService
     {
         //Task<Response<AuthDto>> Login(LoginDto userLoginDto, CancellationToken cancellationToken);
-        //Task<bool> Register(LoginDto model);
+        Task<bool> Register(LoginDto model);
         Task<Response<string>> GenerateJwtToken(string username, CancellationToken cancellationToken);
         //Task<Response<string>> DecodeToken(string token, CancellationToken cancellationToken);
         //Task<Response<bool>> VerifyTokenAsync(string token, CancellationToken cancellationToken = default);
