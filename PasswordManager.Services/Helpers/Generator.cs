0d020009217613de9e493fe6676247f2c8b4c466
Sead Xhigoli <sead.xhigoli1@gmail.com>
2024-11-30 13:14:35 +0100

    remove unnecessary usings from Generator

--- PasswordManager.Services/Helpers/Generator.cs
+++ PasswordManager.Services/Helpers/Generator.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using PasswordManager.Core.Shared;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.Security.Cryptography;
 
 namespace PasswordManager.Services.Helpers
 {
